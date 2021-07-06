505ebc1296c30d5ffb3ffdeb962635a1c21ac616
LeTiina <tiina.j.le@gmail.com>
2021-07-06 10:39:16 +0300

    First version of program

--- tiinan_ja_johannan_quiz.py
+++ tiinan_ja_johannan_quiz.py
@@ -1,1 +1,17 @@
 #tehdään tänne hieno ohjelma
+
+pelataanko = input("Haluatko pelata? (kyllä,ei)")
+kysymykset = {"Onko kissat kivoja?":"kyllä", "Onko koirat kivoja?":"kyllä"}
+
+while(pelataanko == "kyllä"):
+    pisteet = 0
+    for kysymys,vastaus in kysymykset:
+        pelaajan_vastaus = input(kysymys)
+            if pelaajan_vastaus == vastaus:
+                pisteet += 1
+                print("Oikea vastaus!")
+            else:
+                print("Väärä vastaus")
+    
+
+
